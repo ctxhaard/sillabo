353e0a7d79fc07464389195e1d508d6239d3df4f
Carlo Tomasin <c.tomasin@gmail.com>
2021-10-05 21:43:17 +0200

    added vue

--- sillabo.js
+++ sillabo.js
@@ -1,12 +1,23 @@
 
 
+const VOCALI = Object.freeze(['a', 'e', 'i', 'o', 'u', '\''])
+const CONSONANTI = Object.freeze([
+  'b', 'c', 'd', 'f', 'g', 'h',
+  'j,', 'y', 'l', 'm', 'n', 'p',
+  'q', 'r', 's', 't', 'v', 'x',
+  'y', 'w', 'z'])
+
 export function syllables( text ) {
   return [
     [
-      {
-        word: 'cane',
-        syllables: [ 'ca', 'ne']
-      }
+      splitWord( text )
     ]
   ]
+}
+
+function splitWord( word ) {
+  return {
+    word: "cane",
+    syllables: ["ca", "ne"]
+  }
 }
