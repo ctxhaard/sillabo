6676fed5509de85f7e41f4692be428f1020896fa
Carlo Tomasin <carlo.tomasin@icare-world.com>
2021-10-22 23:06:48 +0200

    first test passing

--- __tests__/sillabo.test.js
+++ __tests__/sillabo.test.js
@@ -1,24 +1,47 @@
 
-import { syllables } from '../sillabo'
+import { syllables, lines, words } from '../sillabo'
 
 test('splits "cane" into 1 line, 1 word and syllables ca-ne', () => {
-  expect(syllables('cane')).toEqual([
-    [
-      {
-        word: 'cane',
-        syllables: ['ca', 'ne']
-      }
-    ]
-  ])
+  expect(syllables('cane')).toEqual(
+    ['ca', 'ne']
+  )
 })
 
-test('splits "boato" into 1 line, 1 word and syllables bo-a-to', () => {
-  expect(syllables('boato')).toEqual([
-    [
-      {
-        word: 'boato',
-        syllables: ['bo', 'a', 'to']
-      }
-    ]
+test('splits "boato" into syllables bo-a-to', () => {
+  expect(syllables('boato')).toEqual(
+    ['bo', 'a', 'to']
+  )
+})
+
+test('splits a text into lines', () => {
+
+  let text = `
+Nel mezzo del cammin di nostra vita
+
+mi ritrovai in una selva oscura
+ché la retta via era smarrita.
+Ahi, quanto a dir qual'era è cosa dura
+esta selva selvaggia
+e aspra e forte che il pensier rinnova la paura`
+let l = lines( text )
+
+  expect( l ).toEqual([
+    '',
+    'Nel mezzo del cammin di nostra vita',
+    '',
+    'mi ritrovai in una selva oscura',
+    'ché la retta via era smarrita.',
+    'Ahi, quanto a dir qual\'era è cosa dura',
+    'esta selva selvaggia',
+    'e aspra e forte che il pensier rinnova la paura'])
+
+})
+
+test('splits a line into words', () => {
+  let line = 'Ahi, quanto a dir qual\'era è cosa dura'
+  let w = words( line )
+
+  expect( w ).toEqual([
+    'Ahi', 'quanto', 'a', 'dir', 'qual\'', 'era', 'è', 'cosa', 'dura'
   ])
 })
--- sillabo.js
+++ sillabo.js
@@ -1,12 +1,95 @@
 
+function lines( text ) {
+  let result = []
+  let prev = 0
+  for(let i = 0; i < text.length; i++) {
+    if (text[i] == '\n') {
+        result.push( text.slice(prev, i))
+        prev = i+1
+    }
+  }
+  if (text.length > (prev+1)) result.push( text.slice(prev))
+  return result
+}
 
-export function syllables( text ) {
-  return [
-    [
-      {
-        word: 'cane',
-        syllables: [ 'ca', 'ne']
-      }
-    ]
-  ]
+function words( line ) {
+  let result = []
+  let prev = 0
+  for(let i = 0; i < line.length; ++i) {
+    if([' ', ',', ';', '.'].indexOf( line[i] ) >= 0) {
+      if(i > prev) result.push( line.slice(prev, i))
+      prev = i+1
+    } else if('\'' == line[i]) {
+      if(i > prev) result.push( line.slice(prev, i+1))
+      prev = i+1
+    }
+  }
+  if (line.length > (prev+1)) result.push( line.slice(prev))
+  return result
+}
+
+function syllables( word ) {
+  
+  return begin([], word )
+}
+
+const CONSONANTS = Object.freeze([
+  'c', 'n', 't'
+])
+
+function begin( syllables, word ) {
+
+  if (word.length == 1) {
+    syllables.push( word )
+    return syllables
+  }
+
+  if (CONSONANTS.includes( word.slice(0,1)))
+    return consonant(syllables, word.slice(0,1), word.slice(1) )
+  
+  return vocal (syllables, word.slice(0,1), word.slice(1) )
+}
+
+function consonant(syllables, before, next ) {
+  if (next.length == 0) {
+    if (before.length > 0) {
+      syllables.push( before )
+    }
+    return syllables
+  }
+  if (CONSONANTS.includes(next.slice(0,1))) {
+    syllables.push( before )
+    return begin(syllables, next)
+  }
+  return consonantVocal(syllables, before + next.slice(0,1), next.slice(1))
+}
+
+function vocal (syllables, before, next ) {
+  if (next.length == 0) {
+    if (before.length > 0) {
+      syllables.push( before )
+    }
+    return syllables
+  }
+
+  if(CONSONANTS.includes(next.slice(0,1)))
+    return vocalConsonant(syllables, before + next.slice(0,1), next.slice(1))
+  else
+    return begin(syllables, next)
+}
+
+function consonantVocal(syllables, before, next) {
+  syllables.push(before)
+  return begin(syllables, next)
+}
+
+function vocalConsonant(syllables, before, next) {
+  syllables.push(before)
+  return begin(syllables, next)
+}
+
+export {
+  lines,
+  words,
+  syllables,
 }
