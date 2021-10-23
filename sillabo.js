03d1c3d6df85b14925e0bdd79edaae37990019c4
Carlo Tomasin <c.tomasin@gmail.com>
2021-10-23 21:05:14 +0200

    variables shortening

--- sillabo.js
+++ sillabo.js
@@ -40,91 +40,91 @@ const CONSONANTS = Object.freeze([
   'q', 'r', 's', 't', 'v', 'x',
   'y', 'w', 'z'])
 
-function begin( syllables, word ) {
+function begin( syl, word ) {
 
   if (word.length == 1) {
-    syllables.push( word )
-    return syllables
+    syl.push( word )
+    return syl
   }
 
   if (CONSONANTS.includes( word.slice(0,1)))
-    return consonant(syllables, word.slice(0,1), word.slice(1) )
+    return consonant(syl, word.slice(0,1), word.slice(1) )
   
-  return vocal (syllables, word.slice(0,1), word.slice(1) )
+  return vocal (syl, word.slice(0,1), word.slice(1) )
 }
 
-function consonant(syllables, before, next ) {
+function consonant(syl, acc, next ) {
   if (next.length == 0) {
-    if (before.length > 0) {
-      syllables.push( before )
+    if (acc.length > 0) {
+      syl.push( acc )
     }
-    return syllables
+    return syl
   }
   if (CONSONANTS.includes(next.slice(0,1))) {
-    return consonantConsonant(syllables, before+next.slice(0,1), next.slice(1))
+    return consonantConsonant(syl, acc+next.slice(0,1), next.slice(1))
   }
-  return consonantVocal(syllables, before + next.slice(0,1), next.slice(1))
+  return consonantVocal(syl, acc + next.slice(0,1), next.slice(1))
 }
 
-function vocal (syllables, before, next ) {
+function vocal (syl, acc, next ) {
   if (next.length == 0) {
-    if (before.length > 0) {
-      syllables.push( before )
+    if (acc.length > 0) {
+      syl.push( acc )
     }
-    return syllables
+    return syl
   }
 
-  if(VOCALS.includes(before) && CONSONANTS.includes(next.slice(0,1))) {
-    syllables.push(before)
-    return begin(syllables, next);
+  if(VOCALS.includes(acc) && CONSONANTS.includes(next.slice(0,1))) {
+    syl.push(acc)
+    return begin(syl, next);
   }
 
   if(CONSONANTS.includes(next.slice(0,1)))
-    return vocalConsonant(syllables, before + next.slice(0,1), next.slice(1))
+    return vocalConsonant(syl, acc + next.slice(0,1), next.slice(1))
   else
-    return begin(syllables, next)
+    return begin(syl, next)
 }
 
-function consonantVocal(syllables, before, next) {
+function consonantVocal(syl, acc, next) {
   if (next.length == 0) {
-    syllables.push(before)
-    return syllables
+    syl.push(acc)
+    return syl
   }
 
   if( CONSONANTS.includes(next.slice(0,1)))
-    return consonantVocalConsonant(syllables, before + next.slice(0,1), next.slice(1))
+    return consonantVocalConsonant(syl, acc + next.slice(0,1), next.slice(1))
 
-  syllables.push(before)
-  return begin(syllables, next)
+  syl.push(acc)
+  return begin(syl, next)
 }
 
-function consonantConsonant(syllables, before, next) {
+function consonantConsonant(syl, acc, next) {
   if(next.length == 0) {
-    syllables.push(before)
-    return syllables
+    syl.push(acc)
+    return syl
   }
-  syllables.push( before + next.slice(0,1))
-  return begin(syllables, next.slice(1))
+  syl.push( acc + next.slice(0,1))
+  return begin(syl, next.slice(1))
 }
 
-function vocalConsonant(syllables, before, next) {
-  syllables.push(before)
-  return begin(syllables, next)
+function vocalConsonant(syl, acc, next) {
+  syl.push(acc)
+  return begin(syl, next)
 }
 
-function consonantVocalConsonant(syllables, before, next) {
+function consonantVocalConsonant(syl, acc, next) {
   if(next.length == 0) {
-    syllables.push(before)
-    return syllables
+    syl.push(acc)
+    return syl
   }
 
-  if (before.slice(-1) === next.slice(0,1)) {
-    syllables.push(before)
-    return begin(syllables, next)
+  if (acc.slice(-1) === next.slice(0,1)) {
+    syl.push(acc)
+    return begin(syl, next)
   }
 
-  syllables.push(before.slice(0,-1))
-  return begin(syllables, before.slice(-1) + next)
+  syl.push(acc.slice(0,-1))
+  return begin(syl, acc.slice(-1) + next)
 }
 
 export {
